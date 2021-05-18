ac84d1344bcb728e49e183774e812f171342597a
권민채 <minchae075@naver.com>
2021-05-18 21:39:18 +0900

    베스트앨범(푸는중)

---
+++ Level3/src/BestAlbum.java
@@ -0,0 +1,27 @@
+/**
+ * 베스트앨범
+ * https://programmers.co.kr/learn/courses/30/lessons/42579
+ * 
+ * @author minchae
+ * @date 2021.5.18.
+ */
+
+
+public class BestAlbum {
+
+	public static void main(String[] args) {
+		String[] g = {"classic", "pop", "classic", "classic", "pop"};
+		int[] p = {500, 600, 150, 800, 2500};
+		
+		System.out.println(solution(g, p));
+
+	}
+	
+	public static int[] solution(String[] genres, int[] plays) {
+		
+		
+        int[] answer = {};
+        return answer;
+    }
+
+}
