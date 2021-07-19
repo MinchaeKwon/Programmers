3d420dee5fcc1e44108aee8f9b7d37661096c907
권민채 <minchae075@naver.com>
2021-07-20 00:35:15 +0900

    디스크 컨트롤러
    solved

--- Level3/src/DiskController.java
+++ Level3/src/DiskController.java
@@ -20,7 +20,7 @@ class Job implements Comparable<Job> {
 
 	@Override
 	public int compareTo(Job o) {
-		// 소요 시간을 기준으로 오름차순 정렬
+		// 처리 시간을 기준으로 오름차순 정렬
 		return this.working - o.working;
 	}
 }
@@ -40,23 +40,29 @@ public class DiskController {
 
 		int cnt = 0; // 처리된 디스크
 		int idx = 0; // jobs의 인덱스
-		int time = 0; // 특정 작업 끝난 시간
+		int time = 0; // 현재 시간(특정 작업 끝난 시간)
         int answer = 0;
         
+        // 모든 작업이 처리될 때까지 반복
         while (cnt < jobs.length) {
+        	// 특정 작업이 처리되는 동안 들어오는 요청을 큐에 넣어줌
         	while (idx < jobs.length && jobs[idx][0] <= time) {
         		pq.add(new Job(jobs[idx][0], jobs[idx][1]));
+        		idx++;
         	}
         	
         	if (!pq.isEmpty()) {
-        		time = jobs[idx][0];
-        	}
-        	else {
         		Job job = pq.poll();
         		
-        		answer += time + job.request + job.working;
+        		// (처리 시간 + 작업이 끝난 시간 - 요청 시간)을 더함
+        		answer += job.working + time - job.request;
         		time += job.working;
         		cnt++;
+        	}
+        	else {
+        		// 큐가 비어있는 경우 -> 특정 작업이 끝날 때까지 아무 작업도 요청하지 않는 것
+            	// time을 특정 작업의 요청 시간으로 함
+        		time = jobs[idx][0];
         	}
         }
         
