ad36d45b57bd35b8a74ba2975a873f3caa5358b7
권민채 <minchae075@naver.com>
2021-07-30 00:01:21 +0900

    모의고사
    주석 추가

--- Level1/src/MockTest.java
+++ Level1/src/MockTest.java
@@ -26,6 +26,7 @@ public class MockTest {
 		
 		int[] score = new int[3];
 		
+		// 수포자들이 맞추는 문제 개수를 계산
 		for (int i = 0; i < answers.length; i++) {
 			if (answers[i] == first[i % 5]) {
 				score[0]++;
@@ -40,17 +41,19 @@ public class MockTest {
 			}
 		}
 		
+		// 가장 많은 문제를 맞춘 사람의 점수(가장 높은 점수)를 구함
 		int max = Math.max(score[0], Math.max(score[1], score[2]));
 		
 		ArrayList<Integer> result = new ArrayList<>();
 		
+		// i가 0부터 시작하기 때문에 오름차순 정렬이 돼서 리스트에 추가 됨
 		for (int i = 0; i < score.length; i++) {
+			// 가장 높은 점수와 같은 점수인 사람(가장 많은 문제를 맞춘 사람)을 리스트에 추가
 			if (score[i] == max) {
 				result.add(i + 1);
 			}
 		}
 		
-		int[] answer = result.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
