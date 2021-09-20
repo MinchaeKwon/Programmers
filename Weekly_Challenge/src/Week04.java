d1e0e79427aa200c0dccd0e83fd637c8af15283c
권민채 <minchae075@naver.com>
2021-09-21 00:18:44 +0900

    직업군 추천하기
    solved

--- Weekly_Challenge/src/Week04.java
+++ Weekly_Challenge/src/Week04.java
@@ -9,49 +9,74 @@
 import java.util.HashMap;
 
 public class Week04 {
-	
+
+	static HashMap<String, Integer> map = new HashMap<>();
+
 	public static void main(String[] args) {
-		String[] t1 = {"SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", 
-				"HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"};
-		String[] l1 = {"PYTHON", "C++", "SQL"};
-		int[] p1 = {7, 5, 5};
-		
-		String[] t2 = {"SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", 
-				"PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"};
-		String[] l2 = {"JAVA", "JAVASCRIPT"};
-		int[] p2 = {7, 5};
-		
+		String[] t1 = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+				"HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+				"GAME C++ C# JAVASCRIPT C JAVA" };
+		String[] l1 = { "PYTHON", "C++", "SQL" };
+		int[] p1 = { 7, 5, 5 };
+
+		String[] t2 = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+				"HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+				"GAME C++ C# JAVASCRIPT C JAVA" };
+		String[] l2 = { "JAVA", "JAVASCRIPT" };
+		int[] p2 = { 7, 5 };
+
 		System.out.println(solution(t1, l1, p1));
 		System.out.println(solution(t2, l2, p2));
 	}
-	
+
 	public static String solution(String[] table, String[] languages, int[] preference) {
 		HashMap<String, Integer> hs = new HashMap<>();
-		
-		// 사용하는 언어와 선호도 저장
+
+		// 개발자가 사용하는 언어와 선호도 저장
 		for (int i = 0; i < languages.length; i++) {
 			hs.put(languages[i], preference[i]);
 		}
-		
+
+		String answer = "";
+		int max = 0;
+
 		for (int i = 0; i < table.length; i++) {
 			String[] job = table[i].split(" ");
 			String title = job[0];
-			
+
 			int sum = 0;
-			
+
 			for (int j = 1; j < job.length; j++) {
 				String lan = job[j];
+				int point = 0;
 				
-				if (hs.containsKey(lan)) {
-					sum += hs.get(lan);
+				if (j == 1) {
+					point = job.length;
+				} else {
+					point = job.length - j;
 				}
 				
+				// 해당 언어가 개발자가 사용하는 언어일 경우
+				if (hs.containsKey(lan)) {
+					sum += hs.get(lan) * point;
+				}
 			}
-			
+
+			if (max <= sum) {
+				// 점수가 같으면 사전순으로 비교 -> 음수일 경우는 사전순으로 더 빠른 것이기 때문에 넘어감
+				if (max == sum) {
+					if (answer.compareTo(title) < 0) {
+						continue;
+					}
+				}
+
+				max = sum;
+				answer = title;
+			}
+
 		}
-		
-		
-        String answer = "";
-        return answer;
-    }
+
+		return answer;
+	}
+	
 }
