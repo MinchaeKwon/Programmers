6d360f3c538f0a5e2e0b9c65017b792df65eeae7
권민채 <minchae075@naver.com>
2024-03-23 19:50:49 +0900

    도넛과 막대 그래프
    solved

--- Level2/src/DonutStickGraph.java
+++ Level2/src/DonutStickGraph.java
@@ -6,14 +6,9 @@
  * @date 2024. 3. 23.
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class DonutStickGraph {
 	
-	static ArrayList<Integer>[] list;
-	static boolean[] visited;
-	static int[] incoming; // 특정 정점에 들어오는 간선 개수 저장
+	static int[][] exchangeCnt;
 
 	public static void main(String[] args) {
 		 int[][] edges = {{2, 3}, {4, 3}, {1, 1}, {2, 1}};
@@ -29,98 +24,36 @@ public class DonutStickGraph {
 	public static int[] solution(int[][] edges) {
 		int max = 0;
 		
-		for (int i = 0; i < edges.length; i++) {
-			max = Math.max(max, Math.max(edges[i][0], edges[i][1]));
+		for (int[] edge : edges) {
+			max = Math.max(max, Math.max(edge[0], edge[1]));
 		}
 		
-		list = new ArrayList[max + 1];
-		visited = new boolean[max + 1];
-		incoming = new int[max + 1];
+		exchangeCnt = new int[max + 1][2];
 		
-		for (int i = 0; i < list.length; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < edges.length; i++) {
-			int a = edges[i][0];
-			int b = edges[i][1];
+		for (int[] edge : edges) {
+			int a = edge[0];
+			int b = edge[1];
 			
-			list[a].add(b);
-			incoming[b]++;
+			exchangeCnt[a][0]++;
+			exchangeCnt[b][1]++;
 		}
 		
-		int start = findStart();
-		visited[start] = true;
+		int[] answer = new int[4];
 		
-		int total = list[start].size();
+		for (int i = 1; i < exchangeCnt.length; i++) {
+			if (exchangeCnt[i][0] >= 2 && exchangeCnt[i][1] == 0) {
+				answer[0] = i;
+			} else if (exchangeCnt[i][0] == 0 && exchangeCnt[i][1] > 0) {
+				answer[2]++;
+			} else if (exchangeCnt[i][0] >= 2 && exchangeCnt[i][1] >= 2) {
+				answer[3]++;
+			}
+	   
+		}
 		
-		removeStart(start);
-		
-		int stick = countStick(start);
-		int eight = countEight();
-		int donut = total - stick - eight;
-		
-        int[] answer = {start, donut, stick, eight};
+	    answer[1] = (exchangeCnt[answer[0]][0] - answer[2] - answer[3]);
+	    
         return answer;
     }
-	
-	// 시작 정점 찾기
-	private static int findStart() {
-		int result = -1;
-		
-		for (int i = 1; i < list.length; i++) {
-			if (list[i].size() >= 2 && incoming[i] == 0) {
-				result = i;
-				break;
-			}
-		}
-		
-		return result;
-	}
-	
-	// 시작 정점 연결 끊기
-	private static void removeStart(int start) {
-		for (int n : list[start]) {
-			incoming[n]--;
-		}
-		
-		list[start] = new ArrayList<>();
-	}
-	
-	// 막대 그래프 찾기
-	private static int countStick(int start) {
-		int cnt = 0;
-		
-		for (int i = 1; i < list.length; i++) {
-			if (i == start) {
-				continue;
-			}
-			
-			if (list[i].isEmpty()) {
-				visited[i] = true;
-				cnt++;
-			}
-		}
-		
-		return cnt;
-	}
-	
-	// 8자 모양 그래프 찾기
-	private static int countEight() {
-		int cnt = 0;
-		
-		for (int i = 1; i < list.length; i++) {
-			if (visited[i]) {
-				continue;
-			}
-			
-			if (list[i].size() == 2 && incoming[i] == 2) {
-				visited[i] = true;
-				cnt++;
-			}
-		}
-		
-		return cnt;
-	}
 
 }
